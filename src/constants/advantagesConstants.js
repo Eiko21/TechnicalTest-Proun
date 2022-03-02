f714580a8de7547930fae7e7368ac79ed6c28e96
Cris <barcelona1252@gmail.com>
2022-03-02 19:42:27 +0000

    File with the advantages

---
+++ src/constants/advantagesConstants.js
@@ -0,0 +1,24 @@
+import { rotationLeft, shape, paperplane, calendar } from './imagesConstants';
+
+export const advantages = [
+    {
+        image: rotationLeft,
+        title: "Oferta en 24h",
+        description: "No te preocupes de su estado, ni de reparaciones para eneseñarlo",
+    },
+    {
+        image: shape,
+        title: "Pago inmediato",
+        description: "No te preocupes de su estado, ni de reparaciones para eneseñarlo",
+    },
+    {
+        image: paperplane,
+        title: "Ahórrate el papeleo",
+        description: "Si lo necesitas también podemos ayudarte a encontrar tu nuevo hogar",
+    },
+    {
+        image: calendar,
+        title: "Tu eliges la fecha",
+        description: "Si lo necesitas también podemos ayudarte a encontrar tu nuevo hogar",
+    }
+]
